04c7c67c55651821477feeb1031b193c07c5ab1b
MICROLANDMIRABITE <143100396+MICROLANDMIRABITE@users.noreply.github.com>
2023-11-22 22:37:19 +0800

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+      id: 1,
+      name: {
+        firstname: 'Mic Roland',
+        lastname: 'Mirabite',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'mic123',
+      password: 'isko123',
+      usertype: 'Student'
+    },
+    {
+      id: 2,
+      name: {
+        firstname: 'Elsie',
+        lastname: 'Villorejo',
+      },
+      course: 'BSOA',
+      yearAndSection: '3A',
+      username: 'elsie123',
+      password: 'love123',
+      usertype: 'Student'
+    },
+    {
+      id: 3,
+      name: {
+        firstname: 'John',
+        lastname: 'Sarabia',
+      },
+      course: 'BSCS',
+      yearAndSection: '3A',
+      username: 'john123',
+      password: 'gwapo123',
+      usertype: 'Student'
+    },
+    {
+      id: 4,
+      name: {
+        firstname: 'Roldan',
+        lastname: 'Garcia',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'roldan123',
+      password: 'dan123',
+      usertype: 'Student'
+    },
+    {
+      id: 5,
+      name: {
+        firstname: 'Mary',
+        lastname: 'Jane',
+      },
+      course: 'BSIT',
+      yearAndSection: '3A',
+      username: 'mary123',
+      password: 'jane123',
+      usertype: 'Professor'
+    }
+  ]
+  
+  export default data;
